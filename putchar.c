4365aad43dc904e77423e67ac5f4cc23812160d5
Gwinchy <gmunyui254@gmail.com>
2023-08-22 12:52:20 -0700

    No matter what. Grit be your guide

--- putchar.c
+++ putchar.c
@@ -2,8 +2,11 @@
 /**
  * _putchar - writes a char to the standard output
  * @c: the character to be written
+ *
  * Return: On success, returns the number of characters written.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+
 
 int _putchar(char c)
 {
