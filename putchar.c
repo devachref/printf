a4f47b47f170e6fa6c0835049cf5a0a9179a786f
Gwinchy <gmunyui254@gmail.com>
2023-08-22 07:57:09 -0700

    Corrected the piece of err in putchar

--- putchar.c
+++ putchar.c
@@ -1,12 +1,9 @@
 #include <unistd.h>
 /**
- * _putchar: writes a char to the standard output
+ * _putchar - writes a char to the standard output
  * @c: the character to be written
- *
  * Return: On success, returns the number of characters written.
- * On error, -1 is returned, and errno is set appropriately.
  */
-
 
 int _putchar(char c)
 {
